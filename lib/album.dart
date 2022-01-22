1b32fc0ecf7ae3b483bec2167cd5b985cf53b4b1
rohitsainik <rsainik80@gmail.com>
2022-01-22 20:45:58 +0530

    updated code

--- lib/album.dart
+++ lib/album.dart
@@ -1,25 +1,161 @@
-class Album {
-  final int id;
-  final String email;
-  final String name;
-  final String gender;
-  final String status;
+// To parse this JSON data, do
+//
+//     final album = albumFromJson(jsonString);
 
-  const Album({
-    required this.id,
-    required this.email,
-    required this.name,
-    required this.gender,
-    required this.status,
+import 'dart:convert';
+
+Album albumFromJson(String str) => Album.fromJson(json.decode(str));
+
+String albumToJson(Album data) => json.encode(data.toJson());
+
+class Album {
+  Album({
+    this.meta,
+    this.data,
   });
 
-  factory Album.fromJson(Map<String, dynamic> json) {
-    return Album(
-      id: json['id'],
-      email: json['email'],
-      name: json['name'],
-      gender: json['gender'],
-      status: json['status'],
-    );
+  Meta? meta;
+  List<Datum>? data;
+
+  factory Album.fromJson(Map<String, dynamic> json) => Album(
+    meta: Meta.fromJson(json["meta"]),
+    data: List<Datum>.from(json["data"].map((x) => Datum.fromJson(x))),
+  );
+
+  Map<String, dynamic> toJson() => {
+    "meta": meta?.toJson(),
+    "data": List<dynamic>.from(data!.map((x) => x.toJson())),
+  };
+}
+
+class Datum {
+  Datum({
+    this.id,
+    this.name,
+    this.email,
+    this.gender,
+    this.status,
+  });
+
+  int? id;
+  String? name;
+  String? email;
+  Gender? gender;
+  Status? status;
+
+  factory Datum.fromJson(Map<String, dynamic> json) => Datum(
+    id: json["id"],
+    name: json["name"],
+    email: json["email"],
+    gender: genderValues.map![json["gender"]],
+    status: statusValues.map![json["status"]],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "id": id,
+    "name": name,
+    "email": email,
+    "gender": genderValues.reverse![gender],
+    "status": statusValues.reverse![status],
+  };
+}
+
+enum Gender { FEMALE, MALE }
+
+final genderValues = EnumValues({
+  "female": Gender.FEMALE,
+  "male": Gender.MALE
+});
+
+enum Status { ACTIVE, INACTIVE }
+
+final statusValues = EnumValues({
+  "active": Status.ACTIVE,
+  "inactive": Status.INACTIVE
+});
+
+class Meta {
+  Meta({
+    required this.pagination,
+  });
+
+  Pagination pagination;
+
+  factory Meta.fromJson(Map<String, dynamic> json) => Meta(
+    pagination: Pagination.fromJson(json["pagination"]),
+  );
+
+  Map<String, dynamic> toJson() => {
+    "pagination": pagination.toJson(),
+  };
+}
+
+class Pagination {
+  Pagination({
+    this.total,
+    this.pages,
+    this.page,
+    this.limit,
+    this.links,
+  });
+
+  int? total;
+  int? pages;
+  int? page;
+  int? limit;
+  Links? links;
+
+  factory Pagination.fromJson(Map<String, dynamic> json) => Pagination(
+    total: json["total"],
+    pages: json["pages"],
+    page: json["page"],
+    limit: json["limit"],
+    links: Links.fromJson(json["links"]),
+  );
+
+  Map<String, dynamic> toJson() => {
+    "total": total,
+    "pages": pages,
+    "page": page,
+    "limit": limit,
+    "links": links?.toJson(),
+  };
+}
+
+class Links {
+  Links({
+    this.previous,
+    this.current,
+    this.next,
+  });
+
+  dynamic previous;
+  String? current;
+  String? next;
+
+  factory Links.fromJson(Map<String, dynamic> json) => Links(
+    previous: json["previous"],
+    current: json["current"],
+    next: json["next"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "previous": previous,
+    "current": current,
+    "next": next,
+  };
+}
+
+class EnumValues<T> {
+  Map<String, T>? map;
+  Map<T, String>? reverseMap;
+
+  EnumValues(this.map);
+
+  Map<T, String>? get reverse {
+    if (reverseMap == null) {
+      reverseMap = map?.map((k, v) => new MapEntry(v, k));
+    }
+    return reverseMap;
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -48,208 +48,217 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context)  {
         // futureAlbum =  fetchAlbum();
     return Scaffold(
-      bottomNavigationBar: BottomNavyBar(
-        selectedIndex: _currentIndex,
-        showElevation: true,
-        itemCornerRadius: 24,
-        curve: Curves.easeIn,
-        onItemSelected: (index) => setState(() => _currentIndex = index),
-        items: <BottomNavyBarItem>[
-          BottomNavyBarItem(
-            icon: const Icon(Icons.home),
-            title: const Text('Home'),
-            activeColor: Colors.red,
-            textAlign: TextAlign.center,
-          ),
-          BottomNavyBarItem(
-            icon: const Icon(Icons.chat),
-            title: const Text('Chat'),
-            activeColor: Colors.purpleAccent,
-            textAlign: TextAlign.center,
-          ),
-          BottomNavyBarItem(
-            icon: const Icon(Icons.mail),
-            title: const Text(
-              'Message',
-            ),
-            activeColor: Colors.pink,
-            textAlign: TextAlign.center,
-          ),
-          BottomNavyBarItem(
-            icon: CircleAvatar(
-              radius: 12,
-              child: Image.network(
-                  'https://www.woolha.com/media/2020/03/eevee.png'),
-            ),
-            title: const Text('Profile'),
-            activeColor: Colors.blue,
-            textAlign: TextAlign.center,
-          ),
-        ],
-      ),
-      body: SafeArea(
-        child: Container(
+      bottomNavigationBar: buildBottomNavyBar(),
+      body: buildBody(),
+    );
+  }
 
-          color: Colors.pink[100],
-          child: Stack(
-            children: [
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Card(
-                        child: Image.network(
-                          'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bmV3JTIwemVhbGFuZCUyMHBlb3BsZXxlbnwwfHwwfHw%3D&w=1000&q=80',
-                          height: 170,
-                          width: 170,
-                        ),
+  BottomNavyBar buildBottomNavyBar() {
+    return BottomNavyBar(
+      selectedIndex: _currentIndex,
+      showElevation: true,
+      itemCornerRadius: 24,
+      curve: Curves.easeIn,
+      onItemSelected: (index) => setState(() => _currentIndex = index),
+      items: <BottomNavyBarItem>[
+        BottomNavyBarItem(
+          icon: const Icon(Icons.home),
+          title: const Text('Home'),
+          activeColor: Colors.red,
+          textAlign: TextAlign.center,
+        ),
+        BottomNavyBarItem(
+          icon: const Icon(Icons.chat),
+          title: const Text('Chat'),
+          activeColor: Colors.purpleAccent,
+          textAlign: TextAlign.center,
+        ),
+        BottomNavyBarItem(
+          icon: const Icon(Icons.mail),
+          title: const Text(
+            'Message',
+          ),
+          activeColor: Colors.pink,
+          textAlign: TextAlign.center,
+        ),
+        BottomNavyBarItem(
+          icon: CircleAvatar(
+            radius: 12,
+            child: Image.network(
+                'https://www.woolha.com/media/2020/03/eevee.png'),
+          ),
+          title: const Text('Profile'),
+          activeColor: Colors.blue,
+          textAlign: TextAlign.center,
+        ),
+      ],
+    );
+  }
+
+  buildBody() {
+    return SafeArea(
+      child: Container(
+
+        color: Colors.pink[100],
+        child: Stack(
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Card(
+                      child: Image.network(
+                        'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bmV3JTIwemVhbGFuZCUyMHBlb3BsZXxlbnwwfHwwfHw%3D&w=1000&q=80',
+                        height: 170,
+                        width: 170,
                       ),
-                      const Text('Uddesh Rajoria',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold, fontSize: 16)),
-                      const Text(
-                        'Gwalior, India',
+                    ),
+                    const Text('Uddesh Rajoria',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 16)),
+                    const Text(
+                      'Gwalior, India',
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    RichText(
+                      text: const TextSpan(
+                        // Note: Styles for TextSpans must be explicitly defined.
+                        // Child text spans will inherit styles from parent
                         style: TextStyle(
                           fontSize: 16.0,
                           fontWeight: FontWeight.bold,
                           color: Colors.grey,
                         ),
+                        children: <TextSpan>[
+                          TextSpan(text: 'Relegion: '),
+                          TextSpan(
+                              text: 'Hindu',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.pink)),
+                        ],
                       ),
-                      RichText(
-                        text: const TextSpan(
-                          // Note: Styles for TextSpans must be explicitly defined.
-                          // Child text spans will inherit styles from parent
-                          style: TextStyle(
-                            fontSize: 16.0,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.grey,
-                          ),
-                          children: <TextSpan>[
-                            TextSpan(text: 'Relegion: '),
-                            TextSpan(
-                                text: 'Hindu',
-                                style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.pink)),
-                          ],
+                    ),
+                    const Card(
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 18.0, vertical: 5),
+                        child: Text(
+                          'My Visitors',
+                          style: TextStyle(color: Colors.white),
                         ),
                       ),
-                      const Card(
-                        child: Padding(
-                          padding: EdgeInsets.symmetric(
-                              horizontal: 18.0, vertical: 5),
-                          child: Text(
-                            'My Visitors',
-                            style: TextStyle(color: Colors.white),
-                          ),
-                        ),
-                        color: Colors.pink,
-                      ),
-                      const SizedBox(
-                        height: 50,
-                      )
-                    ],
-                  ),
-                  FutureBuilder<Album>(
-                    future: futureAlbum,
-                    builder: (context, snapshot) {
-                      print(snapshot.data.toString()+'huluuulljhjh');
-                      if (snapshot.hasData) {
-                        SizedBox(
-                          height: 350,
-                          width: 170,
-                          child: LoopPageView.builder(
-                              scrollDirection: Axis.vertical,
-                              itemCount: 2,
-                              itemBuilder: (_, index) {
-                                return Column(
-                                  mainAxisAlignment: MainAxisAlignment.end,
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Card(
-                                      child: Image.network(
-                                        'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bmV3JTIwemVhbGFuZCUyMHBlb3BsZXxlbnwwfHwwfHw%3D&w=1000&q=80',
-                                        height: 170,
-                                        width: 170,
-                                      ),
+                      color: Colors.pink,
+                    ),
+                    const SizedBox(
+                      height: 50,
+                    )
+                  ],
+                ),
+                FutureBuilder<Album>(
+                  future: futureAlbum,
+                  builder: (context, snapshot) {
+                    print(snapshot.data.toString());
+                    if (snapshot.hasData) {
+                      var data  = snapshot.data?.data;
+                     return SizedBox(
+                        height: 350,
+                        width: 170,
+                        child: LoopPageView.builder(
+                            scrollDirection: Axis.vertical,
+                            itemCount: data?.length ?? 0,
+                            itemBuilder: (_, index) {
+                              return Column(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Card(
+                                    child: Image.network(
+                                      'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bmV3JTIwemVhbGFuZCUyMHBlb3BsZXxlbnwwfHwwfHw%3D&w=1000&q=80',
+                                      height: 170,
+                                      width: 170,
                                     ),
-                                     Text(snapshot.data!.status,
-                                        style: const TextStyle(
-                                            fontWeight: FontWeight.bold,
-                                            fontSize: 16)),
-                                    const Text(
-                                      'Gwalior, India',
-                                      style: TextStyle(
+                                  ),
+                                   Text("${data?[index].name}",
+                                      style: const TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 16)),
+                                  const Text(
+                                    'Gwalior, India',
+                                    style: TextStyle(
+                                      fontSize: 16.0,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.grey,
+                                    ),
+                                  ),
+                                  RichText(
+                                    text:  TextSpan(
+                                      // Note: Styles for TextSpans must be explicitly defined.
+                                      // Child text spans will inherit styles from parent
+                                      style: const TextStyle(
                                         fontSize: 16.0,
                                         fontWeight: FontWeight.bold,
                                         color: Colors.grey,
                                       ),
+                                      children: <TextSpan>[
+                                        TextSpan(text: 'Relegion: '),
+                                        TextSpan(
+                                            text: '${data?[index].status}',
+                                            style: const TextStyle(
+                                                fontWeight: FontWeight.bold,
+                                                color: Colors.pink)),
+                                      ],
                                     ),
-                                    RichText(
-                                      text: const TextSpan(
-                                        // Note: Styles for TextSpans must be explicitly defined.
-                                        // Child text spans will inherit styles from parent
-                                        style: TextStyle(
-                                          fontSize: 16.0,
-                                          fontWeight: FontWeight.bold,
-                                          color: Colors.grey,
-                                        ),
-                                        children: <TextSpan>[
-                                          TextSpan(text: 'Relegion: '),
-                                          TextSpan(
-                                              text: 'Hindu',
-                                              style: TextStyle(
-                                                  fontWeight: FontWeight.bold,
-                                                  color: Colors.pink)),
-                                        ],
+                                  ),
+                                  const Card(
+                                    child: Padding(
+                                      padding: EdgeInsets.symmetric(
+                                          horizontal: 18.0, vertical: 5),
+                                      child: Text(
+                                        'My Visitors',
+                                        style: TextStyle(color: Colors.white),
                                       ),
                                     ),
-                                    const Card(
-                                      child: Padding(
-                                        padding: EdgeInsets.symmetric(
-                                            horizontal: 18.0, vertical: 5),
-                                        child: Text(
-                                          'My Visitors',
-                                          style: TextStyle(color: Colors.white),
-                                        ),
-                                      ),
-                                      color: Colors.pink,
-                                    ),
-                                    const SizedBox(
-                                      height: 50,
-                                    )
-                                  ],
-                                );
-                              }),
-                        );
-                      } else if (snapshot.hasError) {
-                        return Text('${snapshot.error}');
-                      }
-                      return const CircularProgressIndicator();
-                    },
-                  )
-                ],
-              ),
-              Center(
-                  child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: const [
-                  Icon(
-                    Icons.headphones_battery_outlined,
-                    size: 180,
-                  ),
-                  SizedBox(
-                    height: 120,
-                  ),
-                ],
+                                    color: Colors.pink,
+                                  ),
+                                  const SizedBox(
+                                    height: 50,
+                                  )
+                                ],
+                              );
+                            }),
+                      );
+                    } else if (snapshot.hasError) {
+                      return Text('${snapshot.error}');
+                    }
+                    return const CircularProgressIndicator();
+                  },
+                )
+              ],
+            ),
+            Center(
+                child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: const [
+                Icon(
+                  Icons.headphones_battery_outlined,
+                  size: 180,
+                ),
+                SizedBox(
+                  height: 120,
+                ),
+              ],
 
-              )),
-            ],
-          ),
+            )),
+          ],
         ),
       ),
     );
